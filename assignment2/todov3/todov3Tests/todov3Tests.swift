1cd3d57b6b17d1850d61bc2cbc6f2a749642d0af
Peter Malolepszy <pmalolep@mail.sfsu.edu>
2018-11-04 22:02:01 -0800

    tests added

--- assignment2/todov3/todov3Tests/todov3Tests.swift
+++ assignment2/todov3/todov3Tests/todov3Tests.swift
@@ -21,16 +21,55 @@ class todov3Tests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func test_newTask(){
+        let taskCTRL = TaskController()
+        let count = taskCTRL.count()
+        let index = taskCTRL.new()
+        
+        XCTAssert( count + 1 == taskCTRL.count())
+        XCTAssert( index == taskCTRL.count() - 1)
+        XCTAssert( taskCTRL.get(taskCTRL.count()-1).name == "new task")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_delete(){
+        let taskCTRL = TaskController()
+        taskCTRL.new()
+        taskCTRL.remove(0)
+        
+        XCTAssert(taskCTRL.count() == 0)
     }
+    
+    func test_statusSwitch(){
+        let taskCTRL = TaskController()
+        taskCTRL.new(name: "test task", status: false)
+        taskCTRL.set(name: "test task", status: true, index: 0)
+        
+        XCTAssert(taskCTRL.get(0).name == "test task")
+        XCTAssert(taskCTRL.get(0).status == true)
+        
+    }
+    
+    func test_nameSwitch(){
+        let taskCTRL = TaskController()
+        taskCTRL.new(name: "test task", status: false)
+        taskCTRL.set(name: "hello", status: false, index: 0)
+        
+        XCTAssert(taskCTRL.get(0).name == "hello")
+        XCTAssert(taskCTRL.get(0).status == false)
+        
+    }
+    
+    func test_lists(){
+        let taskCTRL = TaskController()
+        taskCTRL.new()
+        taskCTRL.new(name: "test 1", status: true)
+        taskCTRL.new(name: "test 2", status: true)
+        
+        XCTAssert( taskCTRL.completedCount() == 2)
+        XCTAssert( taskCTRL.uncompletedCount() == 1)
+        XCTAssert( taskCTRL.getTruePosition(section: 1, row: 0) == 0)
+    }
+    
     
 }
